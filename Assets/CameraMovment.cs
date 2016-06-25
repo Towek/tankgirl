bff6078aab80f11e21f05228d3343dc8054edfcd
Krzysztof Podobinski <towek21@hotmail.com>
2016-06-25 12:03:20 +0200

    1st commit

---
+++ Assets/CameraMovment.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovment : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public class CamFollow : MonoBehaviour {
+        void Main() {
+            Camera.main.transform.position = transform.position;
+        }
+    }
+}
