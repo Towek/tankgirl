f32013855ac083b7881b0252943501c3ca42e20b
Krzysztof Podobinski <towek21@hotmail.com>
2016-06-26 14:36:19 +0200

    look and move buggy as fuck #2

--- Assets/PlayerMovment.cs
+++ Assets/PlayerMovment.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovment : MonoBehaviour {
+public class PlayerMovment : MonoBehaviour
+{
+    public Rigidbody rb;
+    private Vector3 movement = Vector3.zero;
+    public float speed;
+    private CharacterController controller;
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        CharacterController controller = GetComponent<CharacterController>();
+        movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        movement *= speed;
+        controller.Move(movement * Time.deltaTime);
+    }
 }
