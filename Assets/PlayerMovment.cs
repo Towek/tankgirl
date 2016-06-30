ced13d3e2fcb88880cfee3171fac275a97638072
Krzysztof Podobinski <towek21@hotmail.com>
2016-06-30 22:55:42 +0200

    working vertical nad horizontal with limit

--- Assets/PlayerMovment.cs
+++ Assets/PlayerMovment.cs
@@ -7,13 +7,15 @@ public class PlayerMovment : MonoBehaviour
     public Rigidbody rb;
     public float movementSpeed = 5.0f;
     public float mouseSpeed = 2.0f;
+    public float mouseRange = 60.0f;
     //private
     private Vector3 movement = Vector3.zero;
     private CharacterController cc;
-
+    private float mouseVertical = 0;
     // Use this for initialization
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         cc = GetComponent<CharacterController>();
     }
 
@@ -21,8 +23,12 @@ public class PlayerMovment : MonoBehaviour
     void FixedUpdate()
     {
         //mouse rotate
-        float mouseRotate = Input.GetAxis("Mouse X") * mouseSpeed;
-        transform.Rotate(0, mouseRotate, 0);
+        float mouseHorizontal = Input.GetAxis("Mouse X") * mouseSpeed;
+        transform.Rotate(0, mouseHorizontal, 0);
+
+        mouseVertical -= Input.GetAxis("Mouse Y") * mouseSpeed;
+        mouseVertical = Mathf.Clamp(mouseVertical, -mouseRange, mouseRange);
+        Camera.main.transform.localRotation = Quaternion.Euler(mouseVertical, 0, 0);
 
         //movement
         float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
